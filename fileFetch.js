503a67b5cf9ea7a9d909f5d9e9cc6a991d4dc03b
codal-htripathi <htripathi@codal.com>
2024-08-06 15:25:17 +0530

    fixed errors

--- fileFetch.js
+++ fileFetch.js
@@ -1,8 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+
 const STORE_HASH = process.env.STORE_HASH;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-const fetch = require('node-fetch');
-const optionsHtml = require('./scripts/create_script/options.json');
+// Load options.json
+const optionsJsonPath = path.join(__dirname, 'scripts', 'create_script', 'options.json');
+const optionsJson = JSON.parse(fs.readFileSync(optionsJsonPath, 'utf8'));
+
+// Load content.html
+const contentHtmlPath = path.join(__dirname, 'scripts', 'create_script', 'content.html');
+const contentHtml = fs.readFileSync(contentHtmlPath, 'utf8');
+
+// Replace the html field with the actual HTML content
+optionsJson.html = contentHtml;
 
 let url = `https://api.bigcommerce.com/stores/${STORE_HASH}/v3/content/scripts`;
 
@@ -13,7 +25,7 @@ let options = {
     'Content-Type': 'application/json',
     'X-Auth-Token': ACCESS_TOKEN
   },
-  body: JSON.stringify(optionsHtml)
+  body: JSON.stringify(optionsJson)
 };
 
 fetch(url, options)
