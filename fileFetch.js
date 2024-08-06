7d5cbe9df75fd52b99f36c7edfb8667ef0c2ed94
codal-htripathi <htripathi@codal.com>
2024-08-06 15:01:25 +0530

    crete-scropt

---
+++ fileFetch.js
@@ -0,0 +1,22 @@
+const STORE_HASH = process.env.STORE_HASH;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+const fetch = require('node-fetch');
+const optionsHtml = require('./scripts/create_script/options.json');
+
+let url = `https://api.bigcommerce.com/stores/${STORE_HASH}/v3/content/scripts`;
+
+let options = {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'X-Auth-Token': ACCESS_TOKEN
+  },
+  body: JSON.stringify(optionsHtml)
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
