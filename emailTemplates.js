ae2cdeb4317ef889f2cdb29a03453f1de7c4f27a
codal-htripathi <htripathi@codal.com>
2024-08-12 15:31:35 +0530

    emailTemplates4

--- emailTemplates.js
+++ emailTemplates.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const STORE_HASH = process.env.STORE_HASH;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-let url = `https://api.bigcommerce.com/stores/${STORE_HASH}/v3/marketing/emails/account_reset_password_email`;
+let url = `https://api.bigcommerce.com/stores/${STORE_HASH}/v3/marketing/account_reset_password_email`;
 
 let options = {
     method: 'PUT',
@@ -11,22 +11,10 @@ let options = {
       'Content-Type': 'application/json',
       'X-Auth-Token': ACCESS_TOKEN
     },
-    body: JSON.stringify({
-        type_id: "account_reset_password_email",
-        body: `<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title>Title</title> </head> <body> <p>{{lang "reset_password" name=store.name}}</p> <br/> <br/> <a href="{{account.reset_password_link}}">{{account.reset_password_link}}</a> </body> </html>`,
-        translations: [
-            {
-                locale: "en",
-                keys: {
-                    reset_password: "Harshit Tripathi To change your customer account password at {{store.name}} please click this link or copy and paste it into your browser:"
-                }
-            }
-        ],
-        subject: "Harshit Tripathi Reset your password at {{store.name}}"
-    })
-};
+    body: '{"type_id":"account_reset_password_email","body":"<!DOCTYPE html> <html lang=\"en\"> <head>     <meta charset=\"UTF-8\">     <title>Title</title> </head> <body> <p>     {{lang \"reset_password\" name=store.name}} </p> <br/> <br/> <a href=\"{{account.reset_password_link}}\">     {{account.reset_password_link}} </a>  </body> </html>","translations":[{"locale":"en","keys":{"reset_password":"Harshit Tripathi To change your customer account password at {{name}} please click this link or copy and paste it into your browser:"}}],"subject":"Harshit Tripathi Reset your password at {{store.name}}"}'
+  };
 
 fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error("error:" + err));
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.error("error:" + err));
