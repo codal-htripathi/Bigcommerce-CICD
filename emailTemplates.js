c548557afba683b52ff50d10db9988625f6ea903
codal-htripathi <htripathi@codal.com>
2024-08-12 16:55:24 +0530

    emailTemplates8

--- emailTemplates.js
+++ emailTemplates.js
@@ -2,15 +2,16 @@ const fetch = require('node-fetch');
 const STORE_HASH = process.env.STORE_HASH;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-let url = `https://api.bigcommerce.com/stores/${STORE_HASH}/v3/marketing/email-templates`;
+let url = `https://api.bigcommerce.com/stores/${STORE_HASH}/v3/marketing/email-templates/account_reset_password_email`;
 
 let options = {
-  method: 'GET',
+  method: 'PUT',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
     'X-Auth-Token': ACCESS_TOKEN
-  }
+  },
+  body: '{"type_id":"account_reset_password_email","body":"<!DOCTYPE html> <html lang=\"en\"> <head>     <meta charset=\"UTF-8\">     <title>Title</title> </head> <body> <p>     {{lang \"reset_password\" name=store.name}} </p> <br/> <br/> <a href=\"{{account.reset_password_link}}\">     {{account.reset_password_link}} </a>  </body> </html>","translations":[{"locale":"en","keys":{"reset_password":"To change your customer account password at {{name}} please click this link or copy and paste it into your browser:"}}],"subject":"Reset your password at {{store.name}}"}'
 };
 
 fetch(url, options)
